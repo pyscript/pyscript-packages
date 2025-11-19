15b5e025c8800e02af9c67bd786c692cb0157828
Nicholas H.Tollervey <ntoll@ntoll.org>
2025-11-19 12:17:26 +0000

    Refactor build_dato to work more efficiently. Added user contributed info about psutil.json.

--- build_data.py
+++ build_data.py
@@ -4,21 +4,28 @@ package support detail page in the PyScript documentation site.
 
 How?
 
-1. It uses requests to fetch and parse the Pyodide package support data JSON
+1. It fetches community contributed package status updates from a
+Google Sheets document (published as CSV) and uses those to override the
+generated data files as needed.
+
+2. It uses requests to fetch and parse the Pyodide package support data JSON
 files found here:
 
 https://raw.githubusercontent.com/pyscript/polyscript/refs/heads/main/rollup/pyodide_graph.json
 
 Then iterates over them to generate individual package JSON files used by
-the package support detail page. These end up in the /data/ directory.
+the package support detail page. These end up in the /api/packages directory.
+If there has not been an update to the supported versions of Pyodide for a
+given package, the existing JSON file is preserved to avoid overwriting any
+community contributed updates.
 
-2. It also grabs the download stats for PyPI and creates a JSON description
+3. It also grabs the download stats for PyPI and creates a JSON description
 including info about the top 100 packages by download count and whether
 they are supported in Pyodide.
 
-3. Finally, it fetches community contributed package status updates from a
-Google Sheets document (published as CSV) and uses those to override the
-generated data files as needed.
+4. Finally, it records when the script was last run to avoid overwriting
+newer community contributed updates, and generates an all.json file
+containing all package data for easy access.
 
 This is a DELIBERATELY simple script without much error handling or
 sophistication. It is intended to be run occasionally by hand to refresh
@@ -35,149 +42,8 @@ import os
 from io import StringIO
 
 
-############################################
-# Step 1: Generate per-package JSON files from Pyodide data.
-############################################
-print("Generating per-package JSON files from Pyodide data...")
-
-
-# Grab the raw JSON data
-response = requests.get(
-    "https://raw.githubusercontent.com/pyscript/polyscript/refs/heads/main/rollup/pyodide_graph.json"
-)
-response.raise_for_status()
-package_data = response.json()
-
-
-# To hold the per-package data to later be turned into JSON files.
-packages = {}
-
-# Iterate over the releases of Pyodide
-for release, package_list in package_data.items():
-    print(f"Processing release: {release}")
-    for package_name, version in package_list.items():
-        print(f"  Processing package: {package_name}")
-        if package_name not in packages:
-            packages[package_name] = {}
-        # Add the supported version of this package for the given release of
-        # Pyodide.
-        packages[package_name][release] = version
-
-
-updated_by = "automated script"
-updated_at = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
-# Write out the per-package JSON files
-for package_name, data in packages.items():
-    response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
-    if response.status_code != 200:
-        print(
-            f"Warning: Could not fetch PyPI metadata for package '{package_name}'. Skipping."
-        )
-        continue
-    pypi_metadata = response.json()
-    summary = pypi_metadata.get("info", {}).get(
-        "summary", "No summary available."
-    )
-    if not summary:
-        # Some packages have an empty string or Noneas summary.
-        summary = "No summary available."
-    pyodide_versions = [f"`{version}`" for version in data.keys()]
-    notes = f"""This package is [officially supported in Pyodide](https://pyodide.org/en/stable/usage/packages-in-pyodide.html).
-
-To use it in PyScript simply add it to the `packages` section of your TOML configuration like this:
-
-```
-packages = ["{package_name}" ]
-```
-
-Or if you're using a JSON configuration, like this:
-
-```
-{{
-    packages: ["{package_name}"]
- }}
-```
-
-Read more about using packages in PyScript [in our documentation](https://docs.pyscript.net/latest/user-guide/configuration/#packages).
-
-Specifically, the following versions of the package are available for the following Pyodide releases:
-
-Pyodide version: package name (version)
-"""
-    notes += "\n".join(
-        [
-            f"* `{k}`: `{package_name} ({data[k]})`"
-            for k in sorted(data.keys(), reverse=True)
-        ]
-    )
-    output = {
-        "status": "green",
-        "notes": notes,
-        "supported_versions": data,
-        "updated_by": updated_by,
-        "updated_at": updated_at,
-        "summary": summary,
-    }
-    filename = os.path.join("api", "package", f"{package_name}.json")
-    print(f"Writing data for package '{package_name}' to '{filename}'")
-    with open(filename, "w") as f:
-        json.dump(output, f, indent=4)
-
 #############################################
-# Step 2: Generate top_100_pypi_packages.json
-#############################################
-print("Generating top_100_pypi_packages.json...")
-
-# HugoVK generates these stats each month.
-url = "https://hugovk.github.io/top-pypi-packages/top-pypi-packages.json"
-response = requests.get(url)
-response.raise_for_status()
-top_pypi_data = response.json()
-
-last_updated = top_pypi_data.get("last_update", "unknown")
-top100 = top_pypi_data.get("rows", [])[:100]
-
-# Create a summary JSON file for the top 100 packages. Include a check of the
-# Pyodide support status from the previously generated data in the /data/
-# directory, if it exists. Otherwise, default to "amber" status.
-summary = {"last_updated": last_updated, "packages": []}
-for entry in top100:
-    package_name = entry.get("project")
-    print("Processing top package: ", package_name)
-    downloads = entry.get("download_count", 0)
-    # Check for support data
-    try:
-        with open(os.path.join("api", "package", f"{package_name}.json"), "r") as f:
-            support_data = json.load(f)
-            status = support_data.get("status", "amber")
-            desc = support_data.get("summary", "No summary available.")
-    except FileNotFoundError:
-        status = "amber"
-        response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
-        if response.status_code == 200:
-            pypi_metadata = response.json()
-            desc = pypi_metadata.get("info", {}).get(
-                "summary", "No summary available."
-            )
-        else:
-            desc = "No summary available."
-    summary["packages"].append(
-        {
-            "package_name": package_name,
-            "downloads": downloads,
-            "status": status,
-            "summary": desc,
-        }
-    )
-
-# Write out the summary JSON file
-with open(os.path.join("api", "top_100_pypi_packages.json"), "w") as f:
-    json.dump(summary, f, indent=4)
-print("Generated top_100_pypi_packages.json")
-
-
-#############################################
-# Step 3: Process community contributed package status updates.
+# Step 1: Process community contributed package status updates.
 #############################################
 print("Processing community contributed package status updates...")
 
@@ -239,6 +105,181 @@ for row in reader:
     )
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
+
+
+############################################
+# Step 2: Generate per-package JSON files from Pyodide data.
+############################################
+print("Generating per-package JSON files from Pyodide data...")
+
+
+# Grab the raw JSON data
+response = requests.get(
+    "https://raw.githubusercontent.com/pyscript/polyscript/refs/heads/main/rollup/pyodide_graph.json"
+)
+response.raise_for_status()
+package_data = response.json()
+
+
+# To hold the per-package data to later be turned into JSON files.
+packages = {}
+
+# Iterate over the releases of Pyodide
+for release, package_list in package_data.items():
+    print(f"Processing release: {release}")
+    for package_name, version in package_list.items():
+        print(f"  Processing package: {package_name}")
+        if package_name not in packages:
+            packages[package_name] = {}
+        # Add the supported version of this package for the given release of
+        # Pyodide.
+        packages[package_name][release] = version
+
+
+updated_by = "automated script"
+updated_at = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+# Write out the per-package JSON files
+for package_name, data in packages.items():
+    # Check if the package already has a JSON file (possibly updated by
+    # community contributions) and preserve any existing notes.
+    filename = os.path.join("api", "package", f"{package_name}.json")
+    try:
+        with open(filename, "r") as f:
+            existing_data = json.load(f)
+        notes = existing_data.get("notes", "")
+        updated_by = existing_data.get("updated_by", updated_by)
+        updated_at = existing_data.get("updated_at", updated_at)
+    except FileNotFoundError:
+        notes = ""
+    # Check if the supported versions of Pyodide have changed; if not, skip
+    # rewriting the file.
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            existing_data = json.load(f)
+        if existing_data.get("supported_versions", {}) == data:
+            print(
+                f"No changes in supported versions for package '{package_name}'. Skipping."
+            )
+            continue
+        else:
+            print(
+                f"Changes detected in supported Pyodide versions for package '{package_name}'. Updating."
+            )
+            notes = ""  # Reset notes to repopulate with updated info.
+    # Fetch the package summary from PyPI
+    response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    if response.status_code != 200:
+        print(
+            f"Warning: Could not fetch PyPI metadata for package '{package_name}'. Skipping."
+        )
+        continue
+    pypi_metadata = response.json()
+    summary = pypi_metadata.get("info", {}).get(
+        "summary", "No summary available."
+    )
+    if not summary:
+        # Some packages have an empty string or None as summary.
+        summary = "No summary available."
+    pyodide_versions = [f"`{version}`" for version in data.keys()]
+    if not notes:
+        notes = f"""This package is [officially supported in Pyodide](https://pyodide.org/en/stable/usage/packages-in-pyodide.html).
+
+To use it in PyScript simply add it to the `packages` section of your TOML configuration like this:
+
+```
+packages = ["{package_name}" ]
+```
+
+Or if you're using a JSON configuration, like this:
+
+```
+{{
+    packages: ["{package_name}"]
+ }}
+```
+
+Read more about using packages in PyScript [in our documentation](https://docs.pyscript.net/latest/user-guide/configuration/#packages).
+
+Specifically, the following versions of the package are available for the following Pyodide releases:
+
+Pyodide version: package name (version)
+"""
+        notes += "\n".join(
+            [
+                f"* `{k}`: `{package_name} ({data[k]})`"
+                for k in sorted(data.keys(), reverse=True)
+            ]
+        )
+    output = {
+        "status": "green",
+        "notes": notes,
+        "supported_versions": data,
+        "updated_by": updated_by,
+        "updated_at": updated_at,
+        "summary": summary,
+    }
+    filename = os.path.join("api", "package", f"{package_name}.json")
+    print(f"Writing data for package '{package_name}' to '{filename}'")
+    with open(filename, "w") as f:
+        json.dump(output, f, indent=4)
+
+#############################################
+# Step 3: Generate top_100_pypi_packages.json
+#############################################
+print("Generating top_100_pypi_packages.json...")
+
+# HugoVK generates these stats each month.
+url = "https://hugovk.github.io/top-pypi-packages/top-pypi-packages.json"
+response = requests.get(url)
+response.raise_for_status()
+top_pypi_data = response.json()
+
+last_updated = top_pypi_data.get("last_update", "unknown")
+top100 = top_pypi_data.get("rows", [])[:100]
+
+# Create a summary JSON file for the top 100 packages. Include a check of the
+# Pyodide support status from the previously generated data in the /data/
+# directory, if it exists. Otherwise, default to "amber" status.
+summary = {"last_updated": last_updated, "packages": []}
+for entry in top100:
+    package_name = entry.get("project")
+    print("Processing top package: ", package_name)
+    downloads = entry.get("download_count", 0)
+    # Check for support data
+    try:
+        with open(
+            os.path.join("api", "package", f"{package_name}.json"), "r"
+        ) as f:
+            support_data = json.load(f)
+            status = support_data.get("status", "amber")
+            desc = support_data.get("summary", "No summary available.")
+    except FileNotFoundError:
+        status = "amber"
+        response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+        if response.status_code == 200:
+            pypi_metadata = response.json()
+            desc = pypi_metadata.get("info", {}).get(
+                "summary", "No summary available."
+            )
+        else:
+            desc = "No summary available."
+    summary["packages"].append(
+        {
+            "package_name": package_name,
+            "downloads": downloads,
+            "status": status,
+            "summary": desc,
+        }
+    )
+
+# Write out the summary JSON file
+with open(os.path.join("api", "top_100_pypi_packages.json"), "w") as f:
+    json.dump(summary, f, indent=4)
+print("Generated top_100_pypi_packages.json")
+
+#############################################
+# Step 4: Record last run time and output all.json
+#############################################
 
 # Record when the script was last run.
 with open(os.path.join("api", "last_run.json"), "w") as f:
